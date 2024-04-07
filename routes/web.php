8d6efe337a0551faf8147f81f4260f8ca8c4473d
illeskalman77 <illeskalman77@gmail.com>
2024-04-07 13:15:07 +0200

    Routerben a fallback (nem lekezelt url -ek) lekezelés

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\CityController;
+use App\Constants\MenuLinks;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get('/cityList', 'App\Http\Controllers\CityController@getCityListToTable');
+Route::get(MenuLinks::TELEPULES_LISTA, 'App\Http\Controllers\CityController@getCityListToTable');
+
+
+
+// ha olyan URL lenne meghívva, amit az előzőekben nem kezeltem le, akkor ezt hívja meg
+Route::fallback([CityController::class, 'getCityListToTable']);
